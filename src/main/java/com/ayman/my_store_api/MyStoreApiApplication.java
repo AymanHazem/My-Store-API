3e955244c104c10251d767fbc40f518027a40a7a
Ayman Hazem <aymooongsr2@gmail.com>
2025-07-06 06:51:31 +0300

    Init Commit

---
+++ src/main/java/com/ayman/my_store_api/MyStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.ayman.my_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayman/my_store_api/MyStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.my_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
