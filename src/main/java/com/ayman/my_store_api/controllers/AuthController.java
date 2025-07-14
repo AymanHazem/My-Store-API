65ac4c9eb045faaf10fb8f51d1cc1b25b9415b81
Ayman Hazem <aymooongsr2@gmail.com>
2025-07-14 08:18:13 +0300

    Add JWT

---
+++ src/main/java/com/ayman/my_store_api/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.ayman.my_store_api.controllers;
+import com.ayman.my_store_api.dtos.JwtResponse;
+import com.ayman.my_store_api.dtos.LogInRequest;
+import com.ayman.my_store_api.services.JwtService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@AllArgsConstructor
+public class AuthController
+{
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> logIn (@Valid @RequestBody LogInRequest request)
+    {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+        var token =  jwtService.generateToken(request.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> badCredentialsHandler ()
+    {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
